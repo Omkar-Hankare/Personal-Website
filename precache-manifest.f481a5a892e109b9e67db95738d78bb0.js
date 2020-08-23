1378d0a83fb272542be56125b15fae089062fe26
Omkar Hankare <omkarhankare1268@gmail.com>
2020-08-23 18:28:53 +0530

    Updates

---
+++ precache-manifest.f481a5a892e109b9e67db95738d78bb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5d5b2110709ad5e072b2c662b5f5b7",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "880fa66ac703e46c5357",
+    "url": "/personal-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "594cd270a7d76373d085",
+    "url": "/personal-website/static/css/main.2881b59e.chunk.css"
+  },
+  {
+    "revision": "880fa66ac703e46c5357",
+    "url": "/personal-website/static/js/2.fb6f86f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/personal-website/static/js/2.fb6f86f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594cd270a7d76373d085",
+    "url": "/personal-website/static/js/main.bd296ebe.chunk.js"
+  },
+  {
+    "revision": "f1d5662313a542fbce64",
+    "url": "/personal-website/static/js/runtime-main.9e472ed7.js"
+  },
+  {
+    "revision": "452ca260427707ec2582e1929e6ebcaa",
+    "url": "/personal-website/static/media/notion.452ca260.png"
+  }
+]);
